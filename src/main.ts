50a36e80f9276d2576de6e18a042def4f2c3bdac
Robert Wurm <robert.wurm@easysol.at>
2018-10-28 14:26:58 +0100

    FIX safari

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import smoothscroll from 'smoothscroll-polyfill';
 
 if (environment.production) {
   enableProdMode();
 }
+
+
+// kick off the polyfill!
+smoothscroll.polyfill();
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
