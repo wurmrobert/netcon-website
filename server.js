7f8903f12bb3d8e74fba29ec4dbc15e9e9216daa
Robert Wurm <robert.wurm@easysol.at>
2018-10-28 12:15:39 +0100

    FIX node server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/easysolutions-website/index.html'));
 });
 
 // Start the app by listening on the default
